f5fffadd451d15537048247df624656e19c00d76
AudiBi <audiaime11@gmail.com>
2025-08-07 11:44:35 -0400

    Modify the run file

--- run.py
+++ run.py
@@ -1,11 +1,13 @@
+import eventlet
+eventlet.monkey_patch()
+
 import os
-from app import create_app, socketio  # ← importe aussi socketio
+from app import create_app, socketio
 
 app = create_app()
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     debug = os.environ.get('FLASK_DEBUG', 'false').lower() == 'true'
-    
-    # Utilisation correcte de socketio.run pour WebSockets
+    # Utilise socketio.run() avec eventlet
     socketio.run(app, debug=debug, host='0.0.0.0', port=port)
